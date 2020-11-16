02de45b5bd8a6c8bbdf723865054a01ec6312403
Wonjae Hwang <wnjhwng@gmail.com>
2020-11-16 14:01:08 -0800

    style and boilerplating for student card

--- src/components/StudentCard.js
+++ src/components/StudentCard.js
@@ -10,7 +10,12 @@ const StudentCard = ({
 }) => {
   return (
     <div className="student-card">
-      <div className="student-card-container"></div>
+      <div className="student-card-container">
+        <div className="student-card-image-container">
+          <img src={""} alt="" />
+        </div>
+        <div className="student-card-deatils-container"></div>
+      </div>
     </div>
   );
 };
