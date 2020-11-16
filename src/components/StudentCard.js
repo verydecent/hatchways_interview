54a5456fdeeabcafc58cc3cb7c3a2d7b80e547b6
Wonjae Hwang <wnjhwng@gmail.com>
2020-11-16 14:06:38 -0800

    set up classNames and structure

--- src/components/StudentCard.js
+++ src/components/StudentCard.js
@@ -14,7 +14,15 @@ const StudentCard = ({
         <div className="student-card-image-container">
           <img src={""} alt="" />
         </div>
-        <div className="student-card-deatils-container"></div>
+        <div className="student-card-deatils-container">
+          <h1 className="student-card-details-name">
+            {firstName} {lastName}
+          </h1>
+          <p className="student-card-details-text">Email: {email}</p>
+          <p className="student-card-details-text">Company: {company}</p>
+          <p className="student-card-details-text">Skill: {skill}</p>
+          <p className="student-card-details-text">Average: {averageGrade}</p>
+        </div>
       </div>
     </div>
   );
