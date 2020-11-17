054e8395e74eeb787cb87b2844c5a5ae8c7b63cc
Wonjae Hwang <wnjhwng@gmail.com>
2020-11-16 21:02:33 -0800

    Add onBlur

--- src/components/TagContainer.js
+++ src/components/TagContainer.js
@@ -35,6 +35,10 @@ class TagContainer extends React.Component {
             name="tagText"
             value={this.state.tagText}
             onChange={this.onChange}
+            onBlur={(e) => {
+              this.props.submitTag(e, this.props.id, this.state.tagText);
+              this.clearState();
+            }}
             placeholder="add a tag"
             className="tag-input"
           />
